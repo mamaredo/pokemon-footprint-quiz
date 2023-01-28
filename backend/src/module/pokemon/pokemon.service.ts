228785588689ee7158f2cebb0adb534884cd1415
D_nishimura <11kakeru30@gmail.com>
2023-01-28 13:02:14 +0900

    add: createQuizListを作成

--- backend/src/module/pokemon/pokemon.service.ts
+++ backend/src/module/pokemon/pokemon.service.ts
@@ -48,39 +48,19 @@ export class PokemonService {
 
     const pokemonList = await Promise.all(resultList);
     await this.pokemonRepository.create(pokemonList);
+  }
 
-    // readdir(join(__dirname, '/../../../assets'), async (err, fileList) => {
-    //   if (err) return;
+  async createQuizList(maxCount: number, randomLimit: number) {
+    const pokemonList = await this.pokemonRepository.getRandomRecordList(
+      maxCount,
+      randomLimit,
+    );
 
-    //   const resultList: Promise<Pokemon>[] = fileList.map(async (file) => {
-    //     const assetsPath = getAssetsPath(file);
-    //     const colorPallet: FootprintsColor | NotFootprintsColor =
-    //       await getColorFromURL(assetsPath);
+    const pokemonQuizList = pokemonList.map((pokemon, i) => ({
+      ...pokemon,
+      isCorrect: !!(i === 0),
+    }));
 
-    //     if (colorPallet[0] === 255) return;
-
-    //     const footprintFileName = getImageName(file);
-    //     const base64Image = convertImageToBase64(assetsPath);
-
-    //     const res = await fetch(
-    //       `https://pokeapi.co/api/v2/pokemon/${footprintFileName}`,
-    //     );
-    //     const {
-    //       id,
-    //       name,
-    //       sprites: { front_default },
-    //     } = await res.json();
-
-    //     return {
-    //       pokedex: String(id),
-    //       name,
-    //       picture: front_default,
-    //       footprint: base64Image,
-    //     };
-    //   });
-
-    //   const pokemonList = await Promise.all(resultList);
-    //   this.pokemonRepository.create(pokemonList);
-    // });
+    return pokemonQuizList;
   }
 }
