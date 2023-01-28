4e17bc9888717ec1836bc52c451114c7e605cbb0
D_nishimura <11kakeru30@gmail.com>
2023-01-28 13:01:34 +0900

    update: ランダム関数を重複のない関数に更新

--- backend/src/module/pokemon/pokemon.repository.ts
+++ backend/src/module/pokemon/pokemon.repository.ts
@@ -1,6 +1,22 @@
 import { Pokemon, Prisma } from '@prisma/client';
 import { prismaClient } from '../../lib/prisma';
 
+const createRandomValue = (randomLimit: number, arr: number[]) => {
+  const randomValue = Math.floor(Math.random() * Math.floor(randomLimit)) + 1;
+
+  if (arr.includes(randomValue)) return createRandomValue(randomLimit, arr);
+  return randomValue;
+};
+
+const createRandomArray = (maxCount: number, randomLimit: number) => {
+  const randomArray = [...Array(maxCount)].fill(0);
+  for (let i = 0; i < maxCount; i++) {
+    randomArray[i] = createRandomValue(randomLimit, randomArray);
+  }
+
+  return randomArray;
+};
+
 export class PokemonRepository {
   async create(data: Pokemon[]) {
     try {
@@ -12,10 +28,11 @@ export class PokemonRepository {
     }
   }
 
-  async getRandomRecordList(maxCount: number, randomLimit: number) {
-    const searchPokdexList = [...Array(maxCount)].map(
-      () => Math.floor(Math.random() * Math.floor(randomLimit)) + 1,
-    );
+  async getRandomRecordList(
+    maxCount: number,
+    randomLimit: number,
+  ): Promise<Pokemon[]> {
+    const searchPokdexList = createRandomArray(maxCount, randomLimit);
 
     const result = searchPokdexList.map(async (pokedex) =>
       prismaClient.pokemon.findMany({
@@ -23,7 +40,14 @@ export class PokemonRepository {
       }),
     );
 
-    return await Promise.all(result);
+    const _result = await Promise.all(result);
+
+    let pokemonList = [];
+    for (const res of _result) {
+      pokemonList = [...pokemonList, ...res];
+    }
+
+    return pokemonList;
   }
 
   async show() {
